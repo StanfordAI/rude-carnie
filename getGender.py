dbcf48f3d6143d0e1829ef4d8d51faf67737f1c9
aime1 <aime1@aime1s-imac.attlocal.net>
2019-03-02 00:55:35 -0800

    can call it from a differnet directory

--- getGender.py
+++ getGender.py
@@ -12,7 +12,12 @@ genderCommand2 = 'python guess.py --class_type gender --model_type inception --m
 genderCommand3 = 'python guess.py --class_type gender --model_type inception --model_dir ../_checkpoints/21936/ --face_detection_type dlib --face_detection_model shape_predictor_68_face_landmarks.dat                --filename ' + sys.argv[1]
 genderCommand4 = 'python guess.py --class_type gender --model_type inception --model_dir ../_checkpoints/21936/                            --face_detection_model ../darkflow/haar/haarcascade_frontalface_default.xml --filename ' + sys.argv[1]
 
-p = subprocess.Popen(genderCommand4, stdout=subprocess.PIPE, shell=True)
+relativePath = sys.argv[2]
+
+genderCommand5 = 'python '+relativePath+'guess.py --class_type gender --model_type inception --model_dir '+relativePath+'../_checkpoints/21936/ --face_detection_type dlib --face_detection_model '+relativePath+'shape_predictor_68_face_landmarks.dat                --filename ' + sys.argv[1]
+genderCommand6 = 'python '+relativePath+'guess.py --class_type gender --model_type inception --model_dir '+relativePath+'../_checkpoints/21936/                            --face_detection_model '+relativePath+'../darkflow/haar/haarcascade_frontalface_default.xml --filename ' + sys.argv[1]
+
+p = subprocess.Popen(genderCommand6, stdout=subprocess.PIPE, shell=True)
  
 ## Talk with date command i.e. read data from stdout and stderr. Store this info in tuple ##
 ## Interact with process: Send data to stdin. Read data from stdout and stderr, until end-of-file is reached.  ##
